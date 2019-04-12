60cfa82656e0d04a930fcc332bb2567667efeda3
isleygriffith <i_griffith@coloradocollege.edu>
2019-04-11 20:34:56 -0600

    Streamlined strategy testing

---
+++ app/src/test/java/com/example/gigpig/SortByAlphabeticalOrderTest.java
@@ -0,0 +1,48 @@
+package com.example.gigpig;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class SortByAlphabeticalOrderTest {
+    @Test
+    public void testSortAlphabetically() {
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        ArrayList<String> tags = new ArrayList<String>();
+        User user = new User();
+
+        Job job = new Job("YOOOOO WASH MY stuff", "needa wash buddy", 3.20, user, tags);
+        Job job1 = new Job("AAAAYOOOOO WASH MY stuff", "needa wash buddy", 3.20, user, tags);
+        Job job2 = new Job("BAAYOOOOO WASH MY stuff", "needa wash buddy", 3.20, user, tags);
+
+        jobs.add(job);
+        jobs.add(job1);
+        jobs.add(job2);
+
+        ArrayList<Job> jobsShouldntBeModified = new ArrayList<Job>(jobs);
+
+        SortingStrategy sao = new SortByAlphabeticalOrder();
+
+
+        ArrayList<Job> actually_sorted = new ArrayList<Job>();
+        actually_sorted.add(job1);
+        actually_sorted.add(job2);
+        actually_sorted.add(job);
+
+        ArrayList<Job> sorted_jobs_boi = sao.sort(jobs);
+
+        assertEquals(sorted_jobs_boi, actually_sorted);
+        assertEquals(jobsShouldntBeModified, jobs);
+
+    }
+
+
+}
--- app/src/test/java/com/example/gigpig/SortByTagsTest.java
+++ app/src/test/java/com/example/gigpig/SortByTagsTest.java
@@ -11,42 +11,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void testSortAlphabetically() {
-        ArrayList<Job> jobs = new ArrayList<Job>();
-        ArrayList<String> tags = new ArrayList<String>();
-        User user = new User();
-
-        Job job = new Job("YOOOOO WASH MY SHIT", "needa wash buddy", 3.20, user, tags);
-        Job job1 = new Job("AAAAYOOOOO WASH MY SHIT", "needa wash buddy", 3.20, user, tags);
-        Job job2 = new Job("BAAYOOOOO WASH MY SHIT", "needa wash buddy", 3.20, user, tags);
-
-        jobs.add(job);
-        jobs.add(job1);
-        jobs.add(job2);
-
-        ArrayList<Job> jobsShouldntBeModified = new ArrayList<Job>(jobs);
-
-        SortingStrategy sao = new SortByAlphabeticalOrder();
-
-
-        ArrayList<Job> actually_sorted = new ArrayList<Job>();
-        actually_sorted.add(job1);
-        actually_sorted.add(job2);
-        actually_sorted.add(job);
-
-        ArrayList<Job> sorted_jobs_boi = sao.sort(jobs);
-
-        assertEquals(sorted_jobs_boi, actually_sorted);
-        assertEquals(jobsShouldntBeModified, jobs);
-
-    }
+public class SortByTagsTest {
 
     @Test
     public void testSortByTags() {
@@ -61,10 +26,8 @@ public class ExampleUnitTest {
         tags.add("wash");
         tags.add("clean");
         ArrayList<String> noTags = new ArrayList<String>();
-        noTags.add("butt");
+        noTags.add("heya");
         noTags.add("pleasehelpmydad");
-
-
 
         Job job = new Job("wash goat", "needa wash buddy", 3.20, user, tags);
         Job job1 = new Job("clean floor", "needa wash buddy", 3.20, user, tags);
@@ -83,17 +46,7 @@ public class ExampleUnitTest {
         SortingStrategy sbt = new SortByTagsStrategy(user.getTags());
         ArrayList<Job> toSort = sbt.sort(jobs);
         assertEquals(testList, toSort);
-//        System.out.println("before:");
-//
-//        for (Job jobz : jobs) {
-//            System.out.println(jobz.getTitle());
-//        }
-//
-//        System.out.println();
-//        System.out.println("after: ");
-//        for (Job jobz : toSort) {
-//            System.out.println(jobz.getTitle());
-//        }
+        assertNotEquals(toSort, jobs);
 
     }
 }
