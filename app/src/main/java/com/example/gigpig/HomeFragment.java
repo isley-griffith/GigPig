b97f720488c3e4ea045f1761207f67bbc4fa2b76
stefan-bay <s_bay@coloradocollege.edu>
2019-04-14 13:02:16 -0600

    Support for Firebase database

--- app/src/main/java/com/example/gigpig/HomeFragment.java
+++ app/src/main/java/com/example/gigpig/HomeFragment.java
@@ -1,16 +1,13 @@
 package com.example.gigpig;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,12 +15,18 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import android.os.AsyncTask;
 
-import java.sql.SQLException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
+
+import static android.content.ContentValues.TAG;
 
 public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
@@ -45,30 +48,28 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         this.jobsList = new ArrayList<Job>();
         this.sortingStrategy = new SortByAlphabeticalOrder();
 
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//        StrictMode.setThreadPolicy(policy);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 
-//        Thread thread = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                Database database = new Database();
-//            }
-//        });
+        myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
 
-//        ConnectTask database = new ConnectTask();
-//        database.execute();
-
-        System.out.println("WE DIDNT CRASHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
-
-//        try {
-//            this.database = new Database();
-//            this.jobsList = this.database.getAvailableJobs("barry_generic");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
-//        System.out.println("\n\n\n\nJOBLIST" + jobsList);
         this.jobsList = this.sortingStrategy.sort(this.jobsList);
 
         return view;
