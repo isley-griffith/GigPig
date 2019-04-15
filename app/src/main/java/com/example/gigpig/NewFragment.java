79983b858753b22a7592244737f6b5c643074887
stefan-bay <s_bay@coloradocollege.edu>
2019-04-14 23:55:33 -0600

    Added map when creating job

--- app/src/main/java/com/example/gigpig/NewFragment.java
+++ app/src/main/java/com/example/gigpig/NewFragment.java
@@ -1,9 +1,12 @@
 package com.example.gigpig;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +15,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class NewFragment extends Fragment {
+public class NewFragment extends Fragment implements OnMapReadyCallback {
 
     TextView newLable;
     TextView success;
@@ -27,11 +42,17 @@ public class NewFragment extends Fragment {
 
     Button createJobButton;
 
+    MapView mapView;
+    GoogleMap googleMap;
+
     Job newJob;
+
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_new, null);
     }
 
@@ -59,9 +80,79 @@ public class NewFragment extends Fragment {
         this.jobPriceInput = getView().findViewById(R.id.jobPriceInput);
         this.jobTagsInput = getView().findViewById(R.id.jobTagsInput);
 
+        this.mapView = getView().findViewById(R.id.mapView);
+        if (this.mapView != null) {
+            this.mapView.onCreate(null);
+            this.mapView.onResume();
+            this.mapView.getMapAsync(this);
+        }
 
         this.newJob = null;
 
+    }
+
+//    @Override
+//    public void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+//        if (mapViewBundle == null) {
+//            mapViewBundle = new Bundle();
+//            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+//        }
+//
+//        this.mapView.onSaveInstanceState(mapViewBundle);
+//    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        this.mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        this.mapView.onStop();
+    }
+    @Override
+    public void onPause() {
+        this.mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        this.mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDestroyView() {
+        this.mapView.onDestroy();
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        this.mapView.onLowMemory();
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        this.googleMap = googleMap;
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        this.googleMap.setMinZoomPreference(12);
+
+        LatLng cc = new LatLng(38.848450, -104.822714);
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cc, 15));
     }
 
     private ArrayList<String> getTags() {
