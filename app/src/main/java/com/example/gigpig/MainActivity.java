3ab740b1d39641774204314e1578fbf072407a7c
pranavsai <pranavsai142@gmail.com>
2019-04-16 22:35:42 -0600

    Made user stay logged in

--- app/src/main/java/com/example/gigpig/MainActivity.java
+++ app/src/main/java/com/example/gigpig/MainActivity.java
@@ -13,14 +13,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
 
     Button signInButton;
-    EditText usernameField;
+    EditText emailField;
     EditText passwordField;
     private FirebaseAuth mAuth;
 
@@ -28,7 +27,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        usernameField = (EditText)findViewById(R.id.username);
+        emailField = (EditText)findViewById(R.id.email);
         passwordField = (EditText)findViewById(R.id.password);
         setContentView(R.layout.activity_login);
 
@@ -44,27 +43,33 @@ public class MainActivity extends Activity {
 
     public void signInButtonOnClick(View view) {
         signInButton = (Button)findViewById(R.id.signin);
-        usernameField = (EditText)findViewById(R.id.username);
+        emailField = (EditText)findViewById(R.id.email);
         passwordField = (EditText)findViewById(R.id.password);
         final TextView message = (TextView)findViewById(R.id.message);
-        String usernameText = usernameField.getText().toString();
-        String passwordText = passwordField.getText().toString();
-
-        mAuth.signInWithEmailAndPassword(usernameText, passwordText)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            message.setText("Success!");
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            message.setText("Failed. Try Again.");
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+        if(email.isEmpty() || password.isEmpty()) {
+            message.setText("Failed. Try again.");
+        }
+        else {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                message.setText("Success!");
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                String exception = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                message.setText(exception);
+                                updateUI(null);
+                            }
                         }
-                    }
-                });
+                    });
+        }
     }
 
     public void signUpButtonClicked(View view) {
--- app/src/main/java/com/example/gigpig/SignUpActivity.java
+++ app/src/main/java/com/example/gigpig/SignUpActivity.java
@@ -12,11 +12,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -49,29 +47,35 @@ public class SignUpActivity extends Activity {
         System.out.println(number);
         final String username = ((EditText)findViewById(R.id.username)).getText().toString();
         System.out.println(username);
-        String tags = ((EditText)findViewById(R.id.email)).getText().toString();
+        String tags = ((EditText)findViewById(R.id.tags)).getText().toString();
         System.out.println(tags);
         final ArrayList<String> tagsArray = new ArrayList<String>(Arrays.asList(tags.split(" ")));
         final String bio = ((EditText)findViewById(R.id.bio)).getText().toString();
         final TextView message = (TextView) findViewById(R.id.message);
-
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            DatabaseHelper.writeNewUser(new User(user.getUid(), firstname, lastname, number, username, tagsArray, bio));
-                            message.setText("Success!");
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            message.setText("Failed. Try again.");
-                            updateUI(null);
+        if(email.isEmpty() || password.isEmpty()) {
+            message.setText("Failed. Try again.");
+        }
+        else {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                DatabaseHelper.writeNewUser(new User(user.getUid(), firstname, lastname, number, username, tagsArray, bio));
+                                message.setText("Success!");
+                                updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                String exception = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                message.setText(exception);
+                                updateUI(null);
+                            }
                         }
-                    }
-                });
+                    });
+        }
+
     }
 
     public void updateUI(FirebaseUser user) {
